1d855d7683cc53a4a2f87d6a9b921e77901fb027
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2020-12-06 18:24:54 -0800

    Update to use recent versions of test.check and test.chuck

--- project.clj
+++ project.clj
@@ -2,6 +2,6 @@
   :description "fuzz testing for alternate data structures"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/test.check "0.8.0"]
-                 [com.gfredericks/test.chuck "0.1.21"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]]}})
+  :dependencies [[org.clojure/test.check "1.1.0"]
+                 [com.gfredericks/test.chuck "0.2.10"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]]}})
